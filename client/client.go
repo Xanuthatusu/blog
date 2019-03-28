8a738f5b1552e56970453ad7f685230ff4e63a18
Anthony George <anthony.george@tcnbroadcasting.com>
2019-03-28 12:11:18 -0600

    Add basic client and some test data

---
+++ client/client.go
@@ -0,0 +1,37 @@
+// Copyright 2019 Anthony George
+
+// Package main implements a client to connect to the basic blog server
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	pb "github.com/xanuthatusu/blog/protos"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:5334", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("Could not connect to server: ", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewBlogClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	post := &pb.Post{
+		Id: 1,
+	}
+
+	res, err := client.GetPost(ctx, post)
+	if err != nil {
+		log.Fatal("Error when trying to get a post: ", err)
+	}
+	log.Info(res)
+}
